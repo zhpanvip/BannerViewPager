1817e0246b737ceece659dccf474f1664468ae18
zhpan <zhangp@oitsme.com>
2017-10-30 19:11:06 +0800

    优化List集合可接受泛型类数据 增加可自定义布局

---
+++ app/src/main/java/com/example/zhpan/circleviewpager/MyViewHolder.java
@@ -0,0 +1,36 @@
+package com.example.zhpan.circleviewpager;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.viewpager.holder.ViewHolder;
+import com.example.viewpager.utils.ImageLoaderUtil;
+
+/**
+ * Created by zhpan on 2017/10/30.
+ * Description:
+ */
+
+public class MyViewHolder implements ViewHolder<Object> {
+    private ImageView mImageView;
+
+    @Override
+    public View createView(Context context) {
+        // 返回页面布局文件
+        View view = LayoutInflater.from(context).inflate(R.layout.banner_item, null);
+        mImageView = (ImageView) view.findViewById(R.id.banner_image);
+        return view;
+    }
+
+    @Override
+    public void onBind(Context context, int position, Object data) {
+        // 数据绑定
+        if (data instanceof Integer)
+            mImageView.setImageResource((Integer) data);
+        else if (data instanceof String) {
+            ImageLoaderUtil.loadImg(mImageView, (String) data);
+        }
+    }
+}
