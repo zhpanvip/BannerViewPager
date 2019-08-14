c82a6103f16c1c46beeddbfce9e630f7a5768dcd
zhangpan <pan.zhang@alo7.com>
2019-08-14 16:47:21 +0800

    Compile LeakCanary to detected Memory Leak,Fix a Memory Leak in NetworkBannerActivity

--- app/src/main/java/com/example/zhpan/circleviewpager/activity/NetworkBannerActivity.java
+++ app/src/main/java/com/example/zhpan/circleviewpager/activity/NetworkBannerActivity.java
@@ -41,4 +41,10 @@ public class NetworkBannerActivity extends RxAppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mBannerViewPager.stopLoop();
+    }
 }
