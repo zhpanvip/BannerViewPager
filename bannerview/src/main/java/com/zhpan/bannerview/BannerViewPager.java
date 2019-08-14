7cd1aff3e49cd81e874261fa0a4a0ffd2b64bb4f
zhangpan <pan.zhang@alo7.com>
2019-08-14 17:29:06 +0800

    add Annotation for IndicatorGravity

--- bannerview/src/main/java/com/zhpan/bannerview/BannerViewPager.java
+++ bannerview/src/main/java/com/zhpan/bannerview/BannerViewPager.java
@@ -26,6 +26,10 @@ import com.zhpan.bannerview.provider.BannerScroller;
 import com.zhpan.bannerview.provider.ViewStyleSetter;
 import com.zhpan.bannerview.view.IndicatorView;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
@@ -471,6 +475,8 @@ public class BannerViewPager<T, VH extends ViewHolder> extends FrameLayout imple
     }
 
     @IntDef({CENTER, START, END})
+    @Retention(RetentionPolicy.SOURCE)
+    @Target(ElementType.PARAMETER)
     public @interface IndicatorGravity {
     }
 }
