03919170f965a272716d58c15b9c10383cefb853
zhpanvip <zhpanvip@outlook.com>
2019-11-12 23:58:07 +0800

    merge master into 2.4.2

--- bannerview/src/main/java/com/zhpan/bannerview/annotation/Visibility.java
+++ bannerview/src/main/java/com/zhpan/bannerview/annotation/Visibility.java
@@ -16,8 +16,7 @@ import static com.zhpan.bannerview.constants.IndicatorGravity.START;
 
 /**
  * <pre>
- *   Created by zhangpan on 2019-10-18.
- *   Description:指示器显示位置
+ *   Created by zhangpan on 2019-11-12.
  * </pre>
  */
 @IntDef({View.VISIBLE, View.INVISIBLE, View.GONE})
