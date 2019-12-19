2c267e4759f6bdfa12cb8dce9921d58eb023a534
zhpanvip <zhpanvip@outlook.com>
2019-12-19 15:24:35 +0800

    Remove ImageLoader

--- app/src/main/java/com/example/zhpan/circleviewpager/App.java
+++ app/src/main/java/com/example/zhpan/circleviewpager/App.java
@@ -2,8 +2,6 @@ package com.example.zhpan.circleviewpager;
 
 import android.app.Application;
 
-import com.example.zhpan.circleviewpager.imageloader.GlideImageLoader;
-import com.example.zhpan.circleviewpager.imageloader.ImageLoaderManager;
 import com.zhpan.idea.utils.Utils;
 
 /**
@@ -17,7 +15,6 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        ImageLoaderManager.getInstance().init(new GlideImageLoader());
         Utils.init(getApplicationContext());
     }
 }
