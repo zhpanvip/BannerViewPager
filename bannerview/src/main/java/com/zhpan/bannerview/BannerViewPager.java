fc4d6ce8ab21b969dbb5a55ccd648da33eabf323
zhpanvip <zhpanvip@outlook.com>
2020-03-24 10:29:16 +0800

    Fix vertical Sliding conflict with ViewPager2

--- bannerview/src/main/java/com/zhpan/bannerview/BannerViewPager.java
+++ bannerview/src/main/java/com/zhpan/bannerview/BannerViewPager.java
@@ -141,6 +141,8 @@ public class BannerViewPager<T, VH extends ViewHolder> extends RelativeLayout im
                     } else {
                         getParent().requestDisallowInterceptTouchEvent(true);
                     }
+                } else if (2 * disX < disY) {
+                    getParent().requestDisallowInterceptTouchEvent(false);
                 }
                 break;
             case MotionEvent.ACTION_UP:
